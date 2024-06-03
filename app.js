a94ad515d799b3b8c6687c911fc9fbb0d33d9358
userName <userEmail>
2024-06-04 02:53:49 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// 在网页加载完成后添加类名，使元素渐现
+window.addEventListener('load', function() {
+  document.querySelector('.logo').classList.add('loaded');
+});
+window.addEventListener('load', function() {
+  document.querySelector('.home-content h1').classList.add('loaded');
+});
+window.addEventListener('load', function() {
+  document.querySelector('.home-content h3').classList.add('loaded');
+});
+window.addEventListener('load', function() {
+  document.querySelector('.home-content p').classList.add('loaded');
+});
+window.addEventListener('load', function() {
+  document.querySelector('.btn-box').classList.add('loaded');
+});
+window.addEventListener('load', function() {
+  document.querySelector('.home-sic').classList.add('loaded');
+});
